7405de836cd04d6fd5b7708242a66c9548edccbf
Chirag Ailani <chiragailani35@gmail.com>
2023-08-05 14:30:30 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectDatabase = () => {
+const connectDatabase = async() => {
     mongoose.set('strictQuery', false);
-    mongoose.connect(process.env.mongo_url).then((data)=>{
+    await mongoose.connect(process.env.mongo_url).then((data)=>{
         console.log(`MongoDB connected with server: ${data.connection.host}`)
     }).catch((err)=>{
         console.log(err)
     })
+    // await mongoose.connect('mongodb://localhost:27017');
 }
 
 module.exports = connectDatabase;
